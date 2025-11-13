8fdb8a63475301719dc28816174ce59f06a6f41f
parameswary-93 <parameswary93v@gmail.com>
2025-11-13 19:01:20 +0530

    Banner added.

--- website_price_restrict/__manifest__.py
+++ website_price_restrict/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Website price hide for non logged in user",
-    'version': '17.0',
+    "version": "1.0.0",
     'category': 'Website',
     'summary': """Website price hide for non logged in user""",
     'description': """For logged-in users, the shop page will display the product price. 
         Additionally, logged-in users will be able to view the price, quantity, and access the "Add To Cart" button. 
         However, for non logged in users, these features will remain hidden.""",
-    'author': 'For',
+    'author': 'Flyers Friday',
+    'images': ['static/description/banner.jpg'],
     'depends': ['website_sale'],
     'data': [
         'views/product_page_template.xml',
